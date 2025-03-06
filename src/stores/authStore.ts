7edad578b9f5aaceb9bbac341ec4002b32633ad6
TaeHwan Kim <rainkdk02@gmail.com>
2025-03-06 14:50:11 +0900

    feat: 로그인 시 사용자 ID 저장 및 로그아웃 시 삭제 기능 추가

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -5,15 +5,21 @@ import axiosApi from '@/config/axiosConfig'
 
 export const useAuthStore = defineStore('auth', () => {
   const token = ref<string | null>(localStorage.getItem('accessToken')) // 새로고침해도 유지
+  const userId = ref<string | null>(localStorage.getItem('userId')) // 새로고침해도 유지
 
   // 로그인 함수
   const login = async (email: string, password: string) => {
     try {
       const response = await axiosApi.post('/login', { email, password })
 
-      if (response.data.token) {
+      if (response.data.token && response.data.user._id) {
         token.value = response.data.token
+        userId.value = response.data.user._id
+
+
         localStorage.setItem('accessToken', token.value) // localStorage에 저장
+        localStorage.setItem('userId', userId.value) // localStorage에 저장
+
         message.success('로그인 성공!')
         return true
       } else {
@@ -30,9 +36,11 @@ export const useAuthStore = defineStore('auth', () => {
   // 로그아웃 함수
   const logout = () => {
     token.value = null
+    userId.value = null
     localStorage.removeItem('accessToken') // 토큰 삭제
+    localStorage.removeItem('userId') // userId 삭제
     message.success('로그아웃 되었습니다.')
   }
 
-  return { token, login, logout }
+  return { token, userId, login, logout }
 })
